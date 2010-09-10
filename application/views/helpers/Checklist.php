2fec3f1d709e7d60653c2fc5ec61f1e9b12d8af8
Marcone Costa <marcone@mineirim.com>
2010-09-09 23:54:17 -0300

    correção do checklist

--- application/views/helpers/Checklist.php
+++ application/views/helpers/Checklist.php
@@ -22,13 +22,16 @@ class Zend_View_Helper_Checklist {
 	 * 
 	 */
 	public function checklist($trimestre) {
-		
+		$model_projetos = new Model_Projetos();
+		$model_objetivos_especificos = new Model_ObjetivosEspecificos();
+		$model_metas =  new Model_Metas();
 	    $schema = Zend_Registry::get('schema');
 	    
-        $where = "id in (SELECT DISTINCT programa_id FROM $schema.metas_trimestres where trimestre=$trimestre AND (percentual>0 or not avaliacao_descritiva is null))";
+        $where = "id in (SELECT DISTINCT programa_id FROM $schema.metas_trimestres where trimestre=$trimestre AND (not percentual>0 or avaliacao_descritiva is null))";
         $model_programas = new Model_Programas();
        
         $programas = $model_programas->fetchAll($where,array('ordem','id'));
+        
         
         $db = Zend_Registry::get('db');
         $html = array();
@@ -36,21 +39,24 @@ class Zend_View_Helper_Checklist {
         {
 		    $html[] = "<li id='programa_$programa->id'><span class=''>";
 		    $html[] = $programa->menu . PHP_EOL."</span>";
-		    $select = $db->select();
-		    $where = "id in (SELECT DISTINCT projeto_id FROM $schema.metas_trimestres where trimestre=$trimestre AND (percentual>0 or not avaliacao_descritiva is null))";
+		    $select = $model_projetos->select();
+		    $where = "id in (SELECT DISTINCT projeto_id FROM $schema.metas_trimestres where trimestre=$trimestre AND (not percentual>0 or avaliacao_descritiva is null))";
 		    $select->reset('where');
-		    $select->where = $where;
+		    $select->where($where);
+		    
 		    $projetos =$programa->findModel_Projetos($select) ;
+		    
 	    	$html[] = "<ul  id='grp_programa_$programa->id'>";
 		    foreach ($projetos as $projeto)
 		    {
 			    $html[] = "<li id='projeto_$projeto->id'><span class='' >";
 	            $html[] = $projeto->menu."</span>" . PHP_EOL;
 	            
-	            $select = $db->select();
-			    $where = "id in (SELECT DISTINCT objetivo_especifico_id FROM $schema.metas_trimestres where trimestre=$trimestre AND (percentual>0 or not avaliacao_descritiva is null))";
+	            $select = $model_objetivos_especificos->select();
+			    $where = "id in (SELECT DISTINCT objetivo_especifico_id FROM $schema.metas_trimestres where trimestre=$trimestre AND (not percentual>0 or avaliacao_descritiva is null))";
 			    $select->reset('where');
-			    $select->where = $where;
+			    $select->where($where);
+			    
 		        $objetivos = $projeto->findModel_ObjetivosEspecificos($select);    
 	            $html[] = "<ul  id='grp_projeto_$projeto->id'>";
 	            foreach ($objetivos as $objetivo)
@@ -58,10 +64,10 @@ class Zend_View_Helper_Checklist {
 	            	$html[] = "<li id='objetivoespecifico_$objetivo->id'><span class='' >";
 	            	$html[] = $objetivo->menu."</span>" . PHP_EOL;
 	            	
-	            	$select = $db->select();
-				    $where = "id in (SELECT DISTINCT meta_id FROM $schema.metas_trimestres where trimestre=$trimestre AND (percentual>0 or not avaliacao_descritiva is null))";
+	            	$select = $model_metas->select();
+				    $where = "id in (SELECT DISTINCT meta_id FROM $schema.metas_trimestres where trimestre=$trimestre AND (not percentual>0 or avaliacao_descritiva is null))";
 				    $select->reset('where');
-				    $select->where = $where;
+				    $select->where($where);
 			        $metas = $objetivo->findModel_Metas($select);
 			        
 			        $html[] = "<ul  id='grp_objetivo_$objetivo->id'>";
