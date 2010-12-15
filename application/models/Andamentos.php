2473bfa7d14f2c9f59c6aceb21537d01cb806a6f
Marcone Costa <marcone@mineirim.com>
2010-12-15 00:25:56 -0200

    Limpeza parcial dos códigos

--- application/models/Andamentos.php
+++ application/models/Andamentos.php
@@ -6,5 +6,9 @@ class Model_Andamentos extends App_DefaultModel
 	protected $_name = 'andamentos';
 	
 	protected $_dependentTables = array('Model_AtividadesHistorico');
+	public function init(){
+		parent::init();
+		$this->_schema = "public";
+	}	
 }
 
